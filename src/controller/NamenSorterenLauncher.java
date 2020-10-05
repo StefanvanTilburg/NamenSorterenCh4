6059e3769809a77fc4bbca69009f87844a5304ee
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-10-05 09:49:46 +0200

    init repo Opdracht 9.2 Text IO

---
+++ src/controller/NamenSorterenLauncher.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * @author Stefan van Tilburg
+ * <p>
+ * Opdracht 9.2 Text IO
+ * <p>
+ * Doel     Importeer 2 tekst bestanden en verwerk deze via ArrayLists, waarbij sortering toegepast wordt
+ */
+public class NamenSorterenLauncher {
+}
